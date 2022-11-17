b98a4ce95efa44f9141f84c4faa4c5b7b203c987
Geo_K <113002223+Pradogeovana07@users.noreply.github.com>
2022-11-16 20:49:35 -0700

    added questions and buttons with selectable answers

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,36 +8,36 @@ const questionsArray = [
     answer: "A"
   },
   {
-    question: "what does JS stand for?",
-    optionA: "JavaScript",
-    optionB: "Just Saying",
-    optionC: "Job Services",
-    optionD: "J Son",
-    answer: "A"
+    question: "How do you add a comment in a JS file??",
+    optionA: "*comment*",
+    optionB: "{*comment*}",
+    optionC: "/comment",
+    optionD: "//comment",
+    answer: "D"
   },
   {
-    question: "what does JS stand for?",
-    optionA: "JavaScript",
-    optionB: "Just Saying",
-    optionC: "Job Services",
-    optionD: "J Son",
-    answer: "A"
+    question: "Which HTML should be used for the largest heading?",
+    optionA: "<h6>",
+    optionB: "<4>",
+    optionC: "<h1",
+    optionD: "<h3",
+    answer: "C"
   },
   {
-    question: "what does JS stand for?",
-    optionA: "JavaScript",
-    optionB: "Just Saying",
-    optionC: "Job Services",
-    optionD: "J Son",
-    answer: "A"
+    question: "What does CSS allow you to do?",
+    optionA: "Structure a webpage",
+    optionB: "Style a webpage",
+    optionC: "Implement complex features to a webpage",
+    optionD: "Create an interactive page",
+    answer: "B"
   },
   {
-    question: "what does JS stand for? - LAST",
-    optionA: "JavaScript",
-    optionB: "Just Saying",
-    optionC: "Job Services",
-    optionD: "J Son",
-    answer: "A"
+    question: "Which of the following is not shown on a deployed website?",
+    optionA: "Contents inside the <body> tag",
+    optionB: "Contents inside the <heading> tag",
+    optionC: "Contents inside the <p> tag",
+    optionD: "Contents inside the <head> tag",
+    answer: "D"
   },
 
 
@@ -48,9 +48,22 @@ const questCont = document.getElementById("question-container")
 const startbutton = document.getElementById("start-button")
 const timer = document.getElementById("timer")
 const quesDisplay = document.getElementById("question-display")
+const buttonA = document.getElementById("A")
+const buttonB = document.getElementById("B")
+const buttonC = document.getElementById("C")
+const buttonD = document.getElementById("D")
+const rightOrWrong = document.getElementById("rightOrWrong")
+
+buttonA.addEventListener("click",evaluateAnswer)
+buttonB.addEventListener("click",evaluateAnswer)
+buttonC.addEventListener("click",evaluateAnswer)
+buttonD.addEventListener("click",evaluateAnswer)
+
 
 var timerObj;
 var timerCounter = questionsArray.length * 10
+var currentQuestion = 0
+var score = 0
 
 questCont.style.display ="none"
 
@@ -70,13 +83,37 @@ startbutton.addEventListener("click",function(){
 })
 
 function displayQuestion(){
-
+  quesDisplay.textContent = questionsArray[currentQuestion].question
+  buttonA.textContent = questionsArray[currentQuestion].optionA
+  buttonB.textContent = questionsArray[currentQuestion].optionB
+  buttonC.textContent = questionsArray[currentQuestion].optionC
+  buttonD.textContent = questionsArray[currentQuestion].optionD
 }
 
 function displaySummary(){
   clearInterval(timerObj)
+  questCont.style.display ="none"
 }
 
+
+function evaluateAnswer(){
+  var userAnswer = this.getAttribute("id")
+  console.log(userAnswer)
+  if(userAnswer === questionsArray[currentQuestion].answer){
+    score++
+    rightOrWrong.textContent = "Great job!"
+  }else{
+    rightOrWrong.textContent = "Sorry, that's the wrong answer."
+    timerCounter-=5
+  }
+  if(currentQuestion<questionsArray.length-1){
+    currentQuestion++;
+    displayQuestion()
+  }else{
+    displaySummary()
+  }
+
+}
 
 
 
