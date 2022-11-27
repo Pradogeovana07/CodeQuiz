925acf43664428ceb87f19eb894db0f2e78891bd
Geo_K <113002223+Pradogeovana07@users.noreply.github.com>
2022-11-26 20:35:22 -0700

    timer stops at the end of quiz

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -19,8 +19,8 @@ const questionsArray = [
     question: "Which HTML should be used for the largest heading?",
     optionA: "<h6>",
     optionB: "<4>",
-    optionC: "<h1>",
-    optionD: "<h3>",
+    optionC: "<h1",
+    optionD: "<h3",
     answer: "C"
   },
   {
@@ -39,64 +39,59 @@ const questionsArray = [
     optionD: "Contents inside the <head> tag",
     answer: "D"
   },
+
+
 ]
 
-//all constants
-const questCont = document.getElementById("question-container");
-const startbutton = document.getElementById("start-button");
-const timer = document.getElementById("timer");
-const quesDisplay = document.getElementById("question-display");
-const buttonA = document.getElementById("A");
-const buttonB = document.getElementById("B");
-const buttonC = document.getElementById("C");
-const buttonD = document.getElementById("D");
-const finalScore = document.getElementById("finalScore");
-const rightOrWrong = document.getElementById("rightOrWrong");
 
-//when a button is clicked it will evaluate whether answer is correct or incorrect
-buttonA.addEventListener("click",evaluateAnswer);
-buttonB.addEventListener("click",evaluateAnswer);
-buttonC.addEventListener("click",evaluateAnswer);
-buttonD.addEventListener("click",evaluateAnswer);
+const questCont = document.getElementById("question-container")
+const startbutton = document.getElementById("start-button")
+const timer = document.getElementById("timer")
+const quesDisplay = document.getElementById("question-display")
+const buttonA = document.getElementById("A")
+const buttonB = document.getElementById("B")
+const buttonC = document.getElementById("C")
+const buttonD = document.getElementById("D")
+const rightOrWrong = document.getElementById("rightOrWrong")
+const finalScore = document.getElementById("finalScore")
 
-//quiz timer
+
+buttonA.addEventListener("click",evaluateAnswer)
+buttonB.addEventListener("click",evaluateAnswer)
+buttonC.addEventListener("click",evaluateAnswer)
+buttonD.addEventListener("click",evaluateAnswer)
+
+
 var timerObj;
-var timerCounter = questionsArray.length * 10;
-var currentQuestion = 0;
-var score = 0 ;
+var timerCounter = questionsArray.length * 10
+var currentQuestion = 0
+var score = 0
 
-questCont.style.display ="none";
-//when quiz ends the finalScore is displayed, time = 0, and questions are hidden
-function displaySummary(){
-  clearInterval(timerObj)
-  questCont.style.display ="none";
-  finalScore.textContent = `You got ${score} out of 5 correct.`;
-}
+
+function stop() {
+  clearInterval(timerObj);
+  }
+  
+
+questCont.style.display ="none"
+
+
 startbutton.addEventListener("click",function(){
-  questCont.style.display ="block";
-  startbutton.style.display ="none"; 
-
+  questCont.style.display ="block"
+  startbutton.style.display ="none" 
   timerObj = setInterval(function(){
-    timer.textContent = "Timer: "+timerCounter;
+    timer.textContent = "Timer: "+timerCounter
     if(timerCounter>1){
       timerCounter--
-      timerObj = setTimeout ("timerCounter", 1000);
-
-      //if user runs out of time the final score will be displayed but does not go below 0
-  //   if(timerCounter === -1){
-  //     displaySummary();
-  //     questCont.style.display ="none";
-  //   document.getElementById("finalScore").innerHTML = `<h2>You got ${score} out of 5 correct.</h2>`;
-
-  //  }
     }else{
-      displaySummary();
-    };
-  },1000);
-  displayQuestion();
-  clearInterval(timerCounter);
+      displaySummary()
+      stop()
+    }
+  },1000)
+  displayQuestion()
   
 })
+
 
 function displayQuestion(){
   quesDisplay.textContent = questionsArray[currentQuestion].question
@@ -106,35 +101,36 @@ function displayQuestion(){
   buttonD.textContent = questionsArray[currentQuestion].optionD
 }
 
-
+function displaySummary(){
+  
+  questCont.style.display ="none"
+  finalScore.textContent = (`you got ${score} out of 5 questions correct.`)
+}
 
 
 function evaluateAnswer(){
-  var userAnswer = this.getAttribute("id");
-  console.log(userAnswer);
+  var userAnswer = this.getAttribute("id")
+  console.log(userAnswer)
   if(userAnswer === questionsArray[currentQuestion].answer){
-    score += 1;
-    rightOrWrong.innerHTML = "Great job!";
+    score++
+    rightOrWrong.textContent = "Great job!"
   }else{
-    rightOrWrong.innerHTML = "Sorry, that's the wrong answer.";
-    timerCounter-=10;;
-    
+    rightOrWrong.textContent = "Sorry, that's the wrong answer."
+    timerCounter-=5
   }
   if(currentQuestion<questionsArray.length-1){
     currentQuestion++;
-    displayQuestion();
-    
-  }else {
-   displaySummary();
-    questCont.style.display ="none";
-    finalScore.textContent = `You got ${score} out of 5 correct.`;
+    displayQuestion()
+  }else{
+    displaySummary()
+    stop()
+
   }
 
 }
 
     
     
-
 
 
 
